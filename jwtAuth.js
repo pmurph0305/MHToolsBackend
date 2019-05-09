1c6db41c4b7683dee280958c137f744bf7a14402
pmurph0305 <pmurph0305@gmail.com>
2019-05-09 17:34:09 -0400

    Change jwt checkAuth to promise. Use new checkAuth in handleSignin w/ auth token

--- jwtAuth.js
+++ jwtAuth.js
@@ -9,19 +9,19 @@ const requireAuthorization = (req, res, next) => {
   } else {
     return redisClient.get(authorization, function(err, reply) {
       if (err || !reply) {
-          return res.status(401).json("Unauthorized Request");
+        return res.status(401).json("Unauthorized Request");
       }
       return next();
-    })
+    });
   }
-}
+};
 
 const authorizeNewSesssion = (email, id) => {
   const token = signAuthToken(email);
   return createRedisToken(token, id)
     .then(() => ({ id, token }))
     .catch(err => {
-      console.log('create token error', err);
+      console.log("create token error", err);
     });
 };
 
@@ -29,25 +29,24 @@ const createRedisToken = (key, value) => {
   return Promise.resolve(redisClient.set(key, value));
 };
 
-// does the response itself as getAsync requires bluebird.
-const checkAuthRespondWithId = (res, token) => {
-  return redisClient.get(token, function(err, reply) {
-    if (reply) {
-      return res.json({id: reply});
-    } else {
-      console.log('Key/Token not found:', err);
-      return res.status(401).json("Unauthorized Request");
-    }
-  })
-}
+const checkAuth = token => {
+  return new Promise(function(resolve, reject) {
+    redisClient.get(token, function(err, reply) {
+      if (reply && !err) {
+        resolve(reply);
+      } else {
+        reject(err);
+      }
+    });
+  });
+};
 
 const signAuthToken = data => {
   return jwt.sign({ data: data }, "secret", { expiresIn: "48h" });
 };
 
-
 module.exports = {
-  checkAuthRespondWithId,
+  checkAuth,
   authorizeNewSesssion,
-  requireAuthorization,
+  requireAuthorization
 };
--- signin.js
+++ signin.js
@@ -3,14 +3,20 @@ const jwtAuth = require("./jwtAuth");
 const handleSignin = (db, crypt) => (req, res) => {
   const { authorization } = req.headers;
   if (authorization) {
-    jwtAuth.checkAuthRespondWithId(res, authorization)
+    jwtAuth
+      .checkAuth(authorization)
+      .then(reply => {
+        res.status(200).json(reply);
+      })
+      .catch(err => {
+        res.status(401).json("Unauthorized Request");
+      });
   } else {
     handleNewSignin(db, crypt, req)
       .then(token => {
         res.status(200).json(token);
       })
       .catch(err => {
-        console.log(err);
         res.status(400).json(err);
       });
   }
@@ -18,29 +24,20 @@ const handleSignin = (db, crypt) => (req, res) => {
 
 const handleNewSignin = (db, bcrypt, req) => {
   const { email, password } = req.body;
-  return db('users')
-    .select("email", "hash")
+  return db("users")
+    .select("email", "id", "hash")
     .where("email", email)
     .then(data => {
       return bcrypt
         .compare(password, data[0].hash)
-        .then(isCorrect => {
-          return isCorrect
-            ? db("users")
-                .select("id")
-                .where("email", email)
-                .then(data => {
-                  return jwtAuth.authorizeNewSesssion(email, data[0].id);
-                })
-                .catch(err => {
-                  console.log(err);
-                  return Promise.reject("DB error" + err)
-                })
-            : Promise.reject("Authorization error");
+        .then(isCorrectPassword => {
+          return isCorrectPassword
+            ? jwtAuth.authorizeNewSesssion(email, data[0].id)
+            : Promise.reject("Invalid password");
         })
         .catch(err => {
           console.log(err);
-          return Promise.reject('bcrypt error');
+          return Promise.reject("bcrypt error");
         });
     })
     .catch(err => {
